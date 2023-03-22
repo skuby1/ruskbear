56fc138366e084c9858efcc1307b697b5ca749a6
skuby1 <128055336+skuby1@users.noreply.github.com>
2023-03-22 12:01:52 -0300

    Add files via upload

---
+++ novo.js
@@ -0,0 +1,18 @@
+function mostrarimagem() {
+    var img = document.getElementById("hatpng");
+    if (img.style.visibility==="hidden"){
+    img.style.visibility = "visible";
+    } else {
+        img.style.visibility  = "hidden";
+    }
+    
+}
+function mostrarimagem2() {
+    var img = document.getElementById("vodka");
+    if (img.style.visibility ==="hidden"){
+    img.style.visibility  = "visible";
+    } else {
+        img.style.visibility  = "hidden";
+    }
+    
+}
